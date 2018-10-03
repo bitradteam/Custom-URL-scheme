2067220114761ad0271a7c8a9de24178fc9fac20
bitradteam <38139156+bitradteam@users.noreply.github.com>
2018-10-03 11:26:20 +0100

    Update LaunchMyApp.js

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -40,7 +40,11 @@
         []);
     }
   }
-
+  
+  window.handleOpenURL = function(url) {
+    alert("handleOpenURL inside plugin executed. goto: "+url);
+    };
+    
   module.exports = launchmyapp;
 
 }());
