0e41f175acc9de8855e57f5704a21804da80db49
bitradteam <38139156+bitradteam@users.noreply.github.com>
2018-10-03 11:26:49 +0100

    Update LaunchMyApp.js

--- www/ios/LaunchMyApp.js
+++ www/ios/LaunchMyApp.js
@@ -47,7 +47,11 @@
         []);
     }
   }
-
+  
+   window.handleOpenURL = function(url) {
+    alert("handleOpenURL inside plugin executed. goto: "+url);
+    };
+ 
   module.exports = launchmyapp;
 
 }());
